5533e64c91c192715d6b5387db09590d761090f5
nitskp <nitinkpandey4@gmail.com>
2022-06-15 17:52:52 +0530

    Mail sending through etheral email and nodemailer

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,64 @@
 import express from "express";
+import ejs from "ejs";
+import nodemailer from "nodemailer";
+import path from "path";
 
 const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("contactForm");
+});
+
+app.post("/", (req, res) => {
+  const { message, name, email } = req.body;
+  const emailContent = `
+    <h3>Contact Info</h3>
+    <ul>
+        <li>Name: ${name}</li>
+        <li>Email: ${email}</li>
+        <li>Message: ${message} </li>
+    </ul>
+    <h3>Message finished</h3>
+  `;
+  // need to see using gmail or other email providers
+  async function main() {
+    let testAccount = await nodemailer.createTestAccount();
+
+    let transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false,
+      auth: {
+        user: testAccount.user,
+        pass: testAccount.pass,
+      },
+    });
+
+    let info = await transporter.sendMail({
+      from: `"Nitin Pandey" <${testAccount.user}>`,
+      to: "nitnkpandey4@gmail.com, nitinkumarpandey2013@gmail.com",
+      subject: "Mail through mail",
+      html: emailContent,
+    });
+
+    console.log("Message sent: %s", info.messageId);
+
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  }
+
+  main().catch((error) => {
+    res.json({
+      error: error,
+    });
+  });
+
+  res.send("email successfull");
 });
 
 const PORT = 3000;
